bece34a402d40308bdf13932c86c9061142fd4f6
BellaTsou <BellaTsou@peichuntsuou-no-MacBook-Air.local>
2020-12-25 17:48:30 +0800

    Hello Everyone:)

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();//產生express application物件
 const port = process.env.PORT || 8888;
 app.get("/", (req, res) => { //當使用者連線到伺服器的根目錄(/)時，做出回應
-    res.end("Hello World");
+    res.end("Hello Everyone:)");
 });
 
 app.get("/test", (req, res) => {//當使用者連線到/test時，做出回應
