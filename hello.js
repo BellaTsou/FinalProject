18ae8c83b197ef9f0b5cfc62f1679f5140f2c0ea
BellaTsou <BellaTsou@peichuntsuou-no-MacBook-Air.local>
2020-12-25 18:02:23 +0800

    edit main page

--- hello.js
+++ hello.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();//產生express application物件
 const port = process.env.PORT || 8888;
 app.get("/", (req, res) => { //當使用者連線到伺服器的根目錄(/)時，做出回應
-    res.end("Hello Everyone:)");
+    res.end("Hello Everyone:)This is main page."+'<br>'+
+    '<a href="/user">Click here into User page</a>');
 });
 
 app.get("/test", (req, res) => {//當使用者連線到/test時，做出回應
